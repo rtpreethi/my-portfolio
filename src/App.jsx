f67059020a1b35ad78d328f65d4330f862b5a9b3
Preethi <preethi.rt@gmail.com>
2025-10-10 14:43:34 -0500

    Update React app: fixed router and base path

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@
 
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import About from "./components/About";
 import Home from "./components/Home";
